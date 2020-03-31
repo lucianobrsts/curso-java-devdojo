c17de5b99d631a7daedda127910347e3268f35d1
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-03-31 19:42:51 -0300

    Adicionado Aula 07 - Operador Resto e Operador de Comparação

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Lista de Exercícios com conteúdo relevante a todo curso Maratona Java DevDojo.
 
 - **05. Primitive Types, Code Conventions And Comments**
 - **06. Primitive Types, Operators And Exercises**
-- **07. Floating Point Numbers**
+- **07. Rest Operator And Comparation Operator**
 - **08. Characters**
 - **09. Booleans**
 - **10. Local Variables**
---
+++ src/br/com/devdojo/variables/aula07/OperadoresComparacao.java
@@ -0,0 +1,15 @@
+package br.com.devdojo.variables.aula07;
+
+public class OperadoresComparacao {
+    public static void main(String[] args) {
+        boolean dezMaiorqueVinte = 10 > 20;
+        System.out.println(dezMaiorqueVinte);
+
+        boolean dezMenorIgualVinte = 10 <= 20;
+        System.out.println(dezMenorIgualVinte);
+
+        System.out.println(5 == 5);
+
+        System.out.println(5 != 5);
+    }
+}
