296fde05a6b293842d1491911b27f4b134d34425
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-05-18 16:52:54 -0300

    Adicionado Aula 199 - Java 8: CompletableFuture Custom Executor

--- README.md
+++ README.md
@@ -279,3 +279,5 @@ Lista de Exercícios com conteúdo relevante a todo curso Maratona Java DevDojo.
 - **196. CompletableFuture 2, CompletableFuture operation**
 - **197. CompletableFuture 3, Asynchronous Exception Handling**
 - **198. CompletableFuture 4, Performance Compared to Streams**
+- **199. CompletableFuture 5, CompletableFuture Custom Executor**
+- **200. CompletableFuture 5, CompletableFuture Custom Executor**
--- src/br/com/devdojo/completablefuture/test/LojaTest2.java
+++ src/br/com/devdojo/completablefuture/test/LojaTest2.java
@@ -2,8 +2,12 @@ package br.com.devdojo.completablefuture.test;
 
 import br.com.devdojo.completablefuture.classes.Loja;
 
+import javax.management.relation.RelationNotFoundException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
@@ -26,15 +30,79 @@ public class LojaTest2 {
                 new Loja("americanas"),
                 new Loja("casasBahia"),
                 new Loja("bastBuy"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
+                new Loja("casasBahia"),
+                new Loja("bastBuy"),
+                new Loja("wwallMart"),
+                new Loja("americanas"),
+                new Loja("casasBahia"),
                 new Loja("wwallMart"),
                 new Loja("americanas"),
                 new Loja("casasBahia"),
                 new Loja("bastBuy"),
                 new Loja("wwallMart"));
+        System.out.println(Runtime.getRuntime().availableProcessors() + " processadores");
+        System.out.println(lojas.size());
 //        acharPrecos(lojas);
         acharPrecos2(lojas);
 //        acharPrecos3(lojas);
         acharPrecos4(lojas);
+
+        //Formula para encontrar Threas
+        //NumeroDeThreads = Nupu * Ucpu * (1+W/C)
+        //Ncpu = numero de cores disponível
+        //Ucpu = quantidde de utilizado da CPU (0 - 1)
+        //W/C = wait time e compute time
+        //Nthreads = 8 * 1 ( 1 + 99) = 800 threads
+
+        final Executor executor = Executors.newFixedThreadPool(Math.min(lojas.size(), 100), r -> {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            return t;
+        });
+
+        acharPrecos4(lojas, executor);
+
+        //captura da quantidade de processadores
     }
 
     //Chamando com Streams
@@ -85,6 +153,25 @@ public class LojaTest2 {
         List<CompletableFuture<String>> precoFuturo = lojas.stream()
                 .map(loja -> CompletableFuture.supplyAsync(
                         () -> String.format("%s o preço é: %.2f", loja.getNome(), loja.getPreco())
+                ))
+                .collect(Collectors.toList());
+        System.out.println("Tempo de invocação: " + (System.currentTimeMillis() - start) + " ms");
+
+        List<String> collect = precoFuturo.stream().map(CompletableFuture::join).collect(Collectors.toList());
+
+        System.out.println("Tempo total: " + (System.currentTimeMillis() - start) + " ms");
+        System.out.println(collect);
+        return collect;
+    }
+
+    //Chamando com CompletableFuture
+    private static List<String> acharPrecos4(List<Loja> lojas, Executor executor) {
+        System.out.println("Completable Future");
+        long start = System.currentTimeMillis();
+
+        List<CompletableFuture<String>> precoFuturo = lojas.stream()
+                .map(loja -> CompletableFuture.supplyAsync(
+                        () -> String.format("%s o preço é: %.2f", loja.getNome(), loja.getPreco(), executor)
                 ))
                 .collect(Collectors.toList());
         System.out.println("Tempo de invocação: " + (System.currentTimeMillis() - start) + " ms");
