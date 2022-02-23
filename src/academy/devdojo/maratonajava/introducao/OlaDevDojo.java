8bceb1de1ed13e746e733428c5a553404da571f8
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-02-22 22:49:03 -0300

    Entendo os comentários

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+
+    /*
+        Isso é um comentário
+        de multipllas
+        linhas
+     */
+
     public static void main(String[] args) {
         System.out.println("KA-ME-KA-ME-HAAAA");
     }
