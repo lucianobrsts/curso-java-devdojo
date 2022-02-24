3fd89d05ff9929ff0076a4e8aee44435e9d2c75f
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-02-24 08:20:06 -0300

    Tipo primitivo aula 01 finalizada

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,9 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
+        //int, doubler, float, char, byte, short, long, boolean
 
+        int idade = 10;
+        System.out.println("A idade eh:" + idade + " anos");
     }
 }
