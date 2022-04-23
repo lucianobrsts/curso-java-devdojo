c00798c857650d4d706c1f31ba4c7bf905cf5d04
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-04-23 13:14:21 -0300

    Operadores Aritméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        //+ - / *
+
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = numero1 / (double)numero2;
+        System.out.println(resultado);
+
+    }
+}
