90330b943b0a8d58e5e794cc20c9a35ff097d32b
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-04 23:03:31 -0300

    estrutura condicional if else

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05Estruturacondicionais02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05Estruturacondicionais02 {
+    public static void main(String[] args) {
+        //idade < 15 categoria infantil
+        //idade >= 15 &&idade <  18 categoria Juvenil
+        //idade >=18 categoria adulto
+        int idade = 12;
+        String categoria;
+
+        if (idade < 15) {
+            categoria = "Categoria Infantil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Categoria Juvenil";
+        } else {
+            categoria = "Categoria Adulto";
+        }
+    }
+
+}
--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -2,17 +2,20 @@ package academy.devdojo.maratonajava.introducao;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
-        int idade = 20;
+        int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
 
         if (isAutorizadoComprarBebida) {
             System.out.println("Autorizado a comprar bebida alcólica");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcólia");
         }
 
-        if(!isAutorizadoComprarBebida)
-        System.out.println("Não autorizado a comprar bebida alcólia");
+        if(!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcólia");
+        }
 
         boolean c = false;
         if(c == true) {
