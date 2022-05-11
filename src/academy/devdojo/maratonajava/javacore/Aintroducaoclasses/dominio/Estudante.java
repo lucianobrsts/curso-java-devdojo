e046116f47c7e7475c447cb53943a48caa86839a
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-10 23:15:28 -0300

    Orientação a objetos part 1

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
 
 public class Estudante {
     public String nome;
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTest01.java
@@ -1,6 +1,6 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.teste;
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
 
-import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
