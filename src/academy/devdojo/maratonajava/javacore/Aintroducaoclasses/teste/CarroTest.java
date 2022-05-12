4da2636f6d1e1e772aff2ff47833e2c5084e4c4a
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-11 22:06:22 -0300

    Exercicio de classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/CarroTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.nome = "Wolksvagem";
+        carro1.modelo = "Fox";
+        carro1.ano = 2020;
+        System.out.println("Nome: " + carro1.nome + " Modelo: " + carro1.modelo + " Ano: " + carro1.ano);
+
+        Carro carro2 = new Carro();
+        carro2.nome = "Chevrolet";
+        carro2.modelo = "Tracker";
+        carro2.ano = 2018;
+        System.out.println("Nome: " + carro2.nome + " Modelo: " + carro2.modelo + " Ano: " + carro2.ano);
+
+    }
+}
