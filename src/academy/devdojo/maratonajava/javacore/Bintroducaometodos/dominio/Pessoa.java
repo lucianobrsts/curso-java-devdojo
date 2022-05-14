60b2226e235a2f28c15e3cd2fbf22f25b776d569
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-13 23:40:01 -0300

    Criado teste classe pessoa

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
@@ -3,4 +3,9 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 public class Pessoa {
     public String nome;
     public int idade;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -1,7 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
 public class PessoaTest01 {
     public static void main(String[] args) {
-
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Jiraya";
+        pessoa.idade = 70;
+        pessoa.imprime();
     }
 }
