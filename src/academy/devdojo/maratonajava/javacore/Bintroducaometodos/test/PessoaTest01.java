f5516c58d70069031bf3e2ea9b721bb45ea33b20
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-14 15:35:09 -0300

    Modificadores de acesso private

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -5,8 +5,11 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
 public class PessoaTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        pessoa.nome = "Jiraya";
-        pessoa.idade = -1;
-        pessoa.imprime();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+//        pessoa.imprime();
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
     }
 }
