e6deb0b09fc346c8cb00eb7139fff4cbc1f0b411
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-16 23:56:11 -0300

    Criado classe jogadorTest

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -2,4 +2,17 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Jogador {
 
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+public class JogadorTest01 {
+}
