f1518b53c6a64b11640d09edec8ea731d16a9970
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-18 22:56:15 -0300

    Associação Bidirecional

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafú");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("---Jogadores ---");
+        jogador.imprime();
+        System.out.println("---Time ---");
+        time.imprime();
+    }
+}
