9c3283cf4f1c016fa7f3e24e58283f0adbfade95
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-21 16:58:28 -0300

    Exercicio Associação

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -2,9 +2,39 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] seminarios;
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    public void imprime() {
+        System.out.println("----- Seminário Uni7 _____");
+        System.out.println("Professor: " + this.nome);
+        if (seminarios == null) return;
+        for (Seminario seminario : seminarios) {
+            System.out.println("Título: " + seminario.getTitulo());
+            System.out.println("Local: " + seminario.getLocal().getEndereco());
+            if (seminario.getAlunos().length == 0 || seminario.getAlunos() == null) continue;
+            System.out.println("----- Alunos _____");
+            for (Aluno aluno : seminario.getAlunos()) {
+                System.out.println("Aluno: " + aluno.getNome() + " idade: " + aluno.getIdade());
+            }
+        }
+
+        System.out.println("--------------------------");
     }
 
     public String getNome() {
@@ -13,5 +43,21 @@ public class Professor {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/AssociacaoTeste.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Aluno;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Local;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Professor;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Seminario;
+
+public class AssociacaoTeste {
+    public static void main(String[] args) {
+        Local local = new Local("Avenida Washington Soares, UNI7");
+        Seminario seminario = new Seminario("Desvendando Java", null, local);
+
+        Aluno aluno = new Aluno("Luciano Brito", 20, seminario);
+        Seminario[] seminariosDisponiveis = {seminario};
+        Professor professor = new Professor("Chico Henrique", "Mestre Jedi", seminariosDisponiveis);
+
+        Aluno[] alunosInscritos = {aluno};
+        seminario.setAlunos(alunosInscritos);
+
+        professor.imprime();
+
+    }
+}
