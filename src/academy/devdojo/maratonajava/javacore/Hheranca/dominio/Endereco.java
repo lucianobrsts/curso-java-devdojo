bb6ec2a2141782c0bb8569b2056423e89e25a4bc
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-22 19:51:31 -0300

    Heranca aula 01

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Endereco.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Endereco {
+    private String rua;
+    private String cep;
+
+    public Endereco() {
+    }
+
+    public Endereco(String rua, String cep) {
+        this.rua = rua;
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Funcionario extends Pessoa {
+    private Double salario;
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+}
