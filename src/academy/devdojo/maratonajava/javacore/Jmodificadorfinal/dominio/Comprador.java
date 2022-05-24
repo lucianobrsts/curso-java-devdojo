8e3ec014185aba1dc6d5b98da164a377ff7b6e10
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-23 22:35:25 -0300

    Modificador final 2

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
-public class Carro {
+public class Comprador {
     private String nome;
-    public static final double VELOCIDADE_MAXIMA = 250;
-    public final Comprador COMPRADOR = new Comprador();
 
-    public final void imprime() {
-        System.out.println(this.nome);
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 
     public String getNome() {
--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTest01.java
@@ -1,11 +1,19 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.teste;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
 
         System.out.println(Carro.VELOCIDADE_MAXIMA);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("Enzo");
+        ferrari.imprime();
     }
 }
