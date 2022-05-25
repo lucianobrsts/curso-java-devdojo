6511d09e7e762c5a75799351d56be55720423c58
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-24 22:44:00 -0300

    Enumeração 4

--- src/academy/devdojo/maratonajava/javacore/kenum/teste/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/kenum/teste/ClienteTest01.java
@@ -13,5 +13,10 @@ public class ClienteTest01 {
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calculaDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calculaDesconto(100));
+
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente);
+        TipoCliente tipoCliente2 = TipoCliente.tipoClienteNomePorRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
