688d6102d0ad8da4c0d6ec2e238daf60ec5fa07e
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-25 23:12:05 -0300

    Multiplas interfaces no Java

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -2,4 +2,8 @@ package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
     public abstract void load();
+
+    default void checkPermission() {
+        System.out.println("Fazendo checagem de permissões");
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/teste/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/teste/DataLoaderTest01.java
@@ -5,10 +5,16 @@ import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
 
 public class DataLoaderTest01 {
     public static void main(String[] args) {
-    DatabaseLoader databaseLoader = new DatabaseLoader();
-    FileLoader fileLoader = new FileLoader();
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        FileLoader fileLoader = new FileLoader();
 
-    databaseLoader.load();
-    fileLoader.load();
+        databaseLoader.load();
+        fileLoader.load();
+
+        databaseLoader.remove();
+        fileLoader.remove();
+
+        databaseLoader.checkPermission();
+        fileLoader.checkPermission();
     }
 }
