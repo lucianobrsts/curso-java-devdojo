b324f966a5bb3d34bf4e26c7ad67e9064bb116f3
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-25 23:25:13 -0300

    Atributos e métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,6 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public class DatabaseLoader implements DataLoader, DataRemover {
+    // private -> default -> protected -> public
+
     @Override
     public void load() {
         System.out.println("Carregando dados do banco de dados");
@@ -14,5 +16,9 @@ public class DatabaseLoader implements DataLoader, DataRemover {
     @Override
     public void checkPermission() {
         System.out.println("Checando permissões no banco de dados");
+    }
+
+    public static void retrieveMaxDataSize() {
+        System.out.println("Dentro do retrieveMaxDataSize na classe DatabaseLoader");
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/teste/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/teste/DataLoaderTest01.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.teste;
 
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DataLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
 
@@ -16,5 +17,8 @@ public class DataLoaderTest01 {
 
         databaseLoader.checkPermission();
         fileLoader.checkPermission();
+
+        DataLoader.retrieveMaxDataSize();
+        DatabaseLoader.retrieveMaxDataSize();
     }
 }
