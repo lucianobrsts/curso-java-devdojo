466f3aa3ee86f2fd702ef79aa295c39db86cd63e
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-26 22:47:13 -0300

    Polimorfismo 4

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
 
@@ -10,6 +11,12 @@ public class CalculadoraImposto {
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Preço: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
+
+        if (produto instanceof Tomate) {
+            String dataValidade = ((Tomate) produto).getDataValidade();
+            System.out.println("Data de Validade: " + dataValidade);
+
+        }
     }
 
 }
