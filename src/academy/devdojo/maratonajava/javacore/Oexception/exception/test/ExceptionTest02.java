cf79ba4f78fd36866220ccfb3e168097d09ddd49
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-05-30 23:53:54 -0300

    Lançando excecões checadas

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -4,17 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExceptionTest02 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		criarNovoArquivo();
 	}
 	
-	private static void criarNovoArquivo() {
+	private static void criarNovoArquivo() throws  IOException{
 		File file = new File("arquivo\\teste.txt");
 		try {
 			boolean isCriado = file.createNewFile();
 			System.out.println("Arquivo criado " + isCriado);
 		} catch (IOException e) {
 			e.printStackTrace();
+			throw e;
 		}
 	}
 }
