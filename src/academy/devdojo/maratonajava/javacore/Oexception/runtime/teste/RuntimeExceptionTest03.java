30f37fe34291f72ad51b342f5309c18964e731f6
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-02 22:23:11 -0300

    Bloco finally

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest03.java
@@ -1,4 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Oexception.runtime.teste;
 
 public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "Conexão aberta";
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }
 }
