e33cb1b9f3c1c3536496a2f0647f4fd7a5d60385
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-02 22:51:31 -0300

    Adicionado teste 04

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Oexception.runtime.teste;
 
-public class RuntimeExceptionTeste01 {
+public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         Object object = null;
         System.out.println(object.toString());
