161ec2e55bc40f188daa26ce6c26fd1fc885372c
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-02 23:23:48 -0300

    Multi catch em linha

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTest04.java
@@ -7,23 +7,17 @@ public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
         } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
 
             try {
                 talvezLanceException();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            } catch (FileNotFoundException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
     }
