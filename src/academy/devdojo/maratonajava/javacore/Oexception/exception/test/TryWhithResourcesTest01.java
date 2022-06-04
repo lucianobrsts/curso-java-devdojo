e99f34aa9a6393354523b2dd5c04355d33295dcb
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-04 15:44:18 -0300

    Try with resources

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/TryWhithResourcesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/TryWhithResourcesTest01.java
@@ -1,19 +1,23 @@
 package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor1;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor2;
+
 import java.io.*;
 
 public class TryWhithResourcesTest01 {
     public static void main(String[] args) {
-
+        lerArquivo();
     }
 
-//    public static void lerArquivo() {
-//        try () {
-//
-//        } catch () {
-//
-//        }
-//    }
+    public static void lerArquivo() {
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void lerArquivo2() {
         Reader reader = null;
