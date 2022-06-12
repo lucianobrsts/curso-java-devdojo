fa597313c8f164ae9863f1f83af58a11b6de3e49
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-12 16:16:18 -0300

    LocalDateTime

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/teste/LocalDateTimeTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.teste;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2022-08-06");
+        LocalTime time = LocalTime.parse("09:45:21");
+        System.out.println(localDateTime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime ldt1 = date.atTime(time);
+        LocalDateTime ldt2 = time.atDate(date);
+        System.out.println(ldt1);
+        System.out.println(ldt2);
+
+    }
+}
