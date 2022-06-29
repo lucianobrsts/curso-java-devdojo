3abed346638861164b0c3dcf289c530e0dc9bb32
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-06-29 11:46:35 -0300

    Finalizando aula paths

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -1,5 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Wnio.test;
 
-public class PathTest01 {
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class PathTest01 {
+	public static void main(String[] args) {
+		Path p1 = Paths.get("D:\\Estudo\\Repositorios\\curso-java-devdojo\\file.txt");
+		Path p2 = Paths.get("D:\\Estudo\\Repositorios\\curso-java-devdojo", "file.txt");
+		Path p3 = Paths.get("D:", "\\Estudo\\Repositorios\\curso-java-devdojo", "file.txt");
+		Path p4 = Paths.get("D:", "Estudo", "Repositorios", "curso-java-devdojo", "file.txt");
+		System.out.println(p1.getFileName());
+		System.out.println(p2.getFileName());
+		System.out.println(p3.getFileName());
+		System.out.println(p4.getFileName());
+	}
 }
