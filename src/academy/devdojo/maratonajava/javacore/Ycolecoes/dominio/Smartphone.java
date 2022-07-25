ae40070b2ee76148c0bf6936174a502ee50ce6b4
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-25 14:11:04 -0300

    Método equals parte 2

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -11,6 +11,25 @@ public class Smartphone {
 		this.marca = marca;
 	}
 
+	// Reflexivo: x.equals(x) tem de ser true para tudo que for diferente de null;
+	// Simétrico: para x e y diferentes de null, se x.equals(y) == true logo,
+	// y.equals(x) == true;
+	// Transitividade: para x, y e z diferentes de null, se x.equals(y) == true, e
+	// x.equals(z) == true logo, y.equals(z) == true;
+	// Consistente: x.equals(x) sempre retorna true se x for diferente de null
+	// para x diferente de null, x.equals(null) tem que retornar false.
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (this == obj)
+			return true;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Smartphone smartphone = (Smartphone) obj;
+		return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+	}
+
 	public String getSerialNumber() {
 		return serialNumber;
 	}
