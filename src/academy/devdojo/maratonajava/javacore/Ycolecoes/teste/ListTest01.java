d90d17c8ba3ceefa240a9236e4637162d968fdd3
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-26 09:26:15 -0300

    usando a interface collection List parte 2

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/ListTest01.java
@@ -6,16 +6,23 @@ import java.util.List;
 public class ListTest01 {
 	public static void main(String[] args) {
 		List<String> nomes = new ArrayList<>(16); // 1.5 java
+		List<String> nomes2 = new ArrayList<>(16); // 1.5 java
+		
 		nomes.add("William");
-		nomes.add("DevDojo Academy");
+		nomes.add("DevDojo");
+		nomes2.add("Suane");
+		nomes2.add("Academy");
+		
+		//System.out.println(nomes.remove("William"));
+		nomes.addAll(nomes2);
 
 		for (String nome : nomes) {
 			System.out.println(nome);
 		}
 		System.out.println("-----------------");
 
-		nomes.add("Suane");
-		for (int i = 0; i < nomes.size(); i++) {
+		int size = nomes.size();
+		for (int i = 0; i < size; i++) {
 			System.out.println(nomes.get(i));
 		}
 	}
