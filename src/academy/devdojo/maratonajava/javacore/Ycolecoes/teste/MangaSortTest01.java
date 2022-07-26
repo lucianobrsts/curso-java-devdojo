1c98fea0f40ddb4ffc63ba4c6f752db5092acbe4
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-26 14:53:37 -0300

    Finalizado método sort parte 2

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/MangaSortTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.teste;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+public class MangaSortTest01 {
+	public static void main(String[] args) {
+		List<Manga> mangas = new ArrayList<>(6);
+		mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9));
+		mangas.add(new Manga(1L, "Bersek", 9.5));
+		mangas.add(new Manga(4L, "Pokemon", 3.2));
+		mangas.add(new Manga(3L, "Attack on titan", 11.20));
+		mangas.add(new Manga(2L, "Dragon Ball Z", 2.99));
+
+		for (Manga manga : mangas) {
+			System.out.println(manga);
+		}
+		
+		System.out.println("-----------------------------------------------");
+
+		Collections.sort(mangas);
+		for (Manga manga : mangas) {
+			System.out.println(manga);
+		}
+	}
+}
