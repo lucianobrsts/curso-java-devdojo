d985f8d481e7dfdd89d4da0dd95652747f272e7e
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-26 11:21:25 -0300

    Adicionado entidade Manga

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -0,0 +1,68 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+import java.util.Objects;
+
+public class Manga {
+
+	private Long id;
+	private String nome;
+	private double preco;
+
+	public Manga(Long id, String nome, double preco) {
+		Objects.requireNonNull(id, "Id não pode ser nulo.");
+		Objects.requireNonNull(nome, "Nome não pode ser nulo.");
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, preco);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Manga other = (Manga) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Double.doubleToLongBits(preco) == Double.doubleToLongBits(other.preco);
+	}
+
+	@Override
+	public String toString() {
+		return "Manga [id=" + id + ", nome=" + nome + ", preco=" + preco + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
