0e2c20c93b540a0d26dbf1b86011751a7a700947
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-26 14:53:06 -0300

    Implementado método compareTo

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
 
 import java.util.Objects;
 
-public class Manga {
+public class Manga implements Comparable<Manga> {
 
 	private Long id;
 	private String nome;
@@ -64,5 +64,25 @@ public class Manga {
 
 	public void setPreco(double preco) {
 		this.preco = preco;
+	}
+
+	@Override
+	public int compareTo(Manga outroManga) {
+		// Ngativo se o this < outroManga
+		// Se o this == outroManga, return 0
+		// Positivo se this > outroManga
+
+//		if(this.id < outroManga.getId() ) {
+//			return -1;
+//		} else if(this.id.equals(outroManga)) {
+//			return 0;
+//		} else {
+//			return 1;
+//		}
+
+		return this.nome.compareTo(outroManga.getNome());
+//		return Double.compare(preco, outroManga.getPreco());
+//		return Double.valueOf(preco).compareTo(outroManga.getPreco());
+//		return this.id.compareTo(outroManga.getId());
 	}
 }
