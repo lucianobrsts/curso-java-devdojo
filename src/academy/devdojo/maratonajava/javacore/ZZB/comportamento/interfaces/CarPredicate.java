8b7b6f07396ef8cd8a98974ecb5d7165194f3f6e
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-08-29 08:04:12 -0300

    Lambdas - Predicate

--- src/academy/devdojo/maratonajava/javacore/ZZB/comportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZB/comportamento/interfaces/CarPredicate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.ZZB.comportamento.interfaces;
 
 import academy.devdojo.maratonajava.javacore.ZZB.comportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
 
     boolean test(Car car);
